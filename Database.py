afe86f5081b82b33a6a43756c8c2707b65897d4e
RubbelDieKatz <felix@tietjen.it>
2017-03-09 12:15:51 +0100

    created database file

--- Database.py
+++ Database.py
@@ -1,9 +1,11 @@
 '''
-Main file for our project.
+Database helper for our project.
 Created by Felix Tietjen on 09-Mar-2017
 '''
 
+
 import io
+import sqlite3
 
 class Guild:
     def __init__(self):
