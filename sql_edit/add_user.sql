2f50cbb38fb64d389cf54d8fc9603dc808f533b1
felix@tietjen.it <felix@tietjen.it>
2017-03-30 10:54:17 +0000

    Added sql file for user adding

---
+++ sql_edit/add_user.sql
@@ -0,0 +1,35 @@
+INSERT INTO `users` (
+    `salutation`,
+    `firstname`,
+    `lastname`,
+    `street`,
+    `zipcode`,
+    `city`,
+    `country`,
+    `birthday`,
+    `job`,
+    `conditions`,
+    `iban`,
+    `created_at`,
+    `updated_at`
+    )
+
+
+
+
+
+VALUES (
+    ?,
+    ?,
+    ?,
+    ?,
+    ?,
+    ?,
+    ?,
+    ?,
+    ?,
+    ?,
+    ?,
+    ?,
+    ?
+);
